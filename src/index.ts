9dc09e30214678814d086ded0fa74ca704d7d09f
Zack Campbell <zajrik@gmail.com>
2020-02-03 15:41:44 -0600

    fix: add a bunch of missing exports

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,12 @@
 export { Logger } from './Logger';
 export { logger } from './LoggerDecorator';
-export { LogFileTransport } from './transports/LogFileTransport';
 export { LoggerCache } from './LoggerCache';
+
+export { LoggerProxy } from './types/LoggerProxy';
+export { Loggable } from './Loggable';
+
+export { LogLevel } from './types/LogLevel';
+export { LoggerTransportData } from './types/LoggerTransportData';
+export { LogFileTransport } from './transports/LogFileTransport';
+
+export { LoggerTransport } from './LoggerTransport';
